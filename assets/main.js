3559f34d98d2118666f51987c833bf9010bf00ae
Luca Mariani <luca.marianj@gmail.com>
2021-02-05 13:11:16 +0100

    Fixes (#15)
    * fix home search
    
    * fixed modulistica link following
    
    * removed margin-bottom for modulistica and comune select

--- assets/main.js
+++
@@ -1,5 +0,0 @@
-$('#comune').on('change', function(event) {
-    console.log( 'selected ' + $(this).val() );
-    //$('#gobutton').attr( 'href', $(this).val() );
-    window.location.href = $(this).val()
-});
