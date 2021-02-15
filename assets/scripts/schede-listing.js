bf3f7153475848ad4a680b7a586b09d74f5cef1c
Luca Mariani <luca.marianj@gmail.com>
2021-02-15 14:47:04 +0100

    WIP: trying to uncheck

--- assets/scripts/schede-listing.js
+++ assets/scripts/schede-listing.js
@@ -1,23 +1,146 @@
 const $schede = $('.scheda-item')
 const $filterDivArrays = $('.listing-accordion-panel')
 
+/**
+ *
+ * @param item
+ * @param prefixClassName
+ * @returns {*}
+ */
+const getClassesStartingWithString = function (item, prefixClassName) {
+    // const prefixClassName = filter.name + '-[a-z]*'
+    const regExp = new RegExp(prefixClassName,'g')
+    // console.log('classes:', item.className.match(regExp))
+    return item.className.match(regExp)
+}
+
 const Filter = function(filterDiv) {
+    // console.log('creating filter for ', filterDiv)
     this.filterDiv = filterDiv
     this.classesArray = []
+    this.name = $(this.filterDiv).data('filtername')
 }
 
 const listing = function() {
 
+    // store the available filters
     const filterArray = []
+    // store the filters name
+    const filterNamesArray = []
+    // store the checkboxes to show
+    const checkboxes = {}
+
+    const getFilterDivByName = function(name) {
+        let div = {}
+        // console.log('getFilterDivByName searching for', name)
+        filterArray.forEach(function(filter) {
+            // console.log('getFilterDivByName comparing with', filter.name)
+            if(filter.name === name) {
+                // console.log('matched with', filter.name)
+                div = filter.filterDiv
+            }
+        })
+        return div
+    }
+
+    /**
+     * Reads the available filters from schede
+     * For each scheda the filters are CSS classes following this
+     * naming convention: filterName_filterValue
+     */
+    const initFilterCheckboxes = function() {
+        initCheckboxes()
+        const $visibleSchede = $('.scheda-item').not('.d-none')
+        $visibleSchede.each(function (index, item) {
+            filterArray.forEach(function (filter) {
+                // console.log('filling checkboxes for filter', filter.name)
+                addFilterCheckboxes(item, filter)
+            })
+        })
+
+        // now cycle on checkboxes arrays to add them in dom
+        Object.keys(checkboxes).forEach(function (checkboxName) {
+            const checkbox = checkboxes[checkboxName]
+            const filterDiv = getFilterDivByName(checkboxName)
+            console.log(checkbox)
+            checkbox.items.forEach(function(item) {
+                const patchedTemplate = patchTemplate(checkboxes[checkboxName].template, filters[checkboxName][item])
+                const $checkboxesContainer = $(filterDiv).find('.collapse-body')
+                // console.log('filterdiv', $checkboxesContainer)
+                $checkboxesContainer.append(patchedTemplate)
+            })
+        })
+    }
+
+    const patchTemplate = function(template, option) {
+        let patched = template.replaceAll('__FILTER_ID__', option.metaname)
+        patched = patched.replaceAll('__FILTER_METANAME__', option.metaname)
+        patched = patched.replaceAll('__FILTER_NAME__', option.name)
+        return patched
+    }
+
+    const addCheckboxInArray = function (filterName, value) {
+        if(checkboxes[filterName]) {
+            if ( ! checkboxes[filterName].items.includes(value)) {
+                checkboxes[filterName].items.push(value)
+            }
+        }
+    }
+
+    const fillCheckboxesArrays = function (filter, cssclasses) {
+        cssclasses.forEach(function (cssclass) {
+            // console.log('adding checkbox for cssclass', cssclass)
+            if(filters[filter.name][cssclass]) {
+                // console.log(patchTemplate(template, filters[filter.name][option]))
+                addCheckboxInArray(filter.name, cssclass)
+            }
+        })
+    }
+
+    /**
+     *
+     * @param filter
+     */
+    const addFilterCheckboxes = function (item, filter) {
+        const prefixClassName = filter.name + '_[a-z]*'
+        const cssclasses = getClassesStartingWithString(item, prefixClassName)
+        // console.log('adding checkboxes for filter', filter.name, ': ', filters[filter.name])
+
+        // get the filter template
+        const template = $(filter.filterDiv).data('filtertemplate')
+        // add the template in checkboxes object
+        checkboxes[filter.name].template = template
+        // fill checkboxes arrays
+        fillCheckboxesArrays(filter, cssclasses)
+    }
+
+    const initCheckboxes = function(filterName) {
+        // init object
+        filterArray.forEach(function (filter) {
+            checkboxes[filter.name] = {}
+            checkboxes[filter.name].items = []
+        })
+        // init DOM
+        Object.keys(checkboxes).forEach(function (checkboxName) {
+            const checkbox = checkboxes[checkboxName]
+            const filterDiv = getFilterDivByName(checkboxName)
+            const $checkboxesContainer = $(filterDiv).find('.collapse-body')
+            $checkboxesContainer.html('')
+        })
+    }
 
     /**
      * initialize filters
      */
-    const initFilterArray = function() {
+    const initFilterArrays = function() {
         $filterDivArrays.each(function (index, item) {
-            filterArray.push(new Filter(item))
+            const filter = new Filter(item)
+            filterArray.push(filter)
+            filterNamesArray.push(filter.name)
+            // addCheckboxProperty(filter.name)
         })
         console.log('filterArray', filterArray)
+        console.log('filterNames', filterNamesArray)
     }
 
     const setSchedeCount = function (count) {
@@ -40,7 +163,14 @@ const listing = function() {
         })
 
         $schede2show.removeClass('d-none')
+
+        // update schde count
         setSchedeCount($schede2show.length)
+
+        // update filters
+        initFilterCheckboxes()
+        // add handlers again
+        addChangeHandlers()
     }
 
     const onChangeCheckbox = function ($item, filter) {
@@ -83,7 +213,8 @@ const listing = function() {
         });
     }
 
-    initFilterArray()
+    initFilterArrays()
+    initFilterCheckboxes()
     addChangeHandlers()
     setSchedeCount($schede.length)
 }
